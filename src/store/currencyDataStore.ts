5e009066d3e4e9d94e56fcd973f35d03b723db23
Aaron Callaghan <aaroncallaghan40@gmail.com>
2025-07-07 08:18:01 +0100

    Removed unused action and comment from example code

--- src/store/currencyDataStore.ts
+++ src/store/currencyDataStore.ts
@@ -29,8 +29,6 @@ const useCurrencyDataStore = create((set) => ({
       if(currencyType===sourceCurrencyType) return ({...state, sourceCurrencyData: {...state.sourceCurrencyData, sourceCurrencyCode: newSourceCurrency}})
       else return ({...state, convertedCurrencyData: {...state.convertedCurrencyData, convertedCurrencyCode: newSourceCurrency}})
     }),
-    setConvertedCurrency: (newConvertedCurrency: string) =>
-      set((state: CurrencyDataState) => ({convertedCurrencyData: {...state.convertedCurrencyData, convertedCurrencyCode: newConvertedCurrency}})),
     setSourceValue: (newSourceValue: number) =>
       set((state: CurrencyDataState) => ({sourceCurrencyData: {...state.sourceCurrencyData, sourceCurrencyValue: newSourceValue}})),
     setConvertedValue: (newConvertedValue: number) =>
@@ -45,6 +43,6 @@ export const useSourceValue = () => useCurrencyDataStore((state) => state.source
 export const useConvertedCurrency = () => useCurrencyDataStore((state) => state.convertedCurrencyData.convertedCurrencyCode);
 export const useConvertedValue = () => useCurrencyDataStore((state) => state.convertedCurrencyData.convertedCurrencyValue);
 export const useHasSelectedValue = (currencyType: CurrencyTypes) => useCurrencyDataStore((state) => (currencyType===sourceCurrencyType)?state.sourceCurrencyData.sourceCurrencyValue!=='':state.convertedCurrencyData.convertedCurrencyValue!=='');
-// 🎉 one selector for all our actions
+
 export const useCurrencyDataActions = () =>
   useCurrencyDataStore((state) => state.actions)
